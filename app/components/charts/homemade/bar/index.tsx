8c516a406b236a1ad459535d19d01f742d8ef588
Robert Zetterlund <robert.zetterlund@outlook.com>
2022-09-04 12:23:28 +0200

    add showmore to stats

--- app/components/charts/homemade/bar/index.tsx
+++ app/components/charts/homemade/bar/index.tsx
@@ -1,6 +1,7 @@
+import { useState, useCallback, useMemo, useRef } from "react";
 import type { ReactNode } from "react";
-import { useCallback, useMemo, useRef } from "react";
 import useIntersectionObserver from "../../../../hooks/useIntersectionObserver";
+import { useSpring, animated } from "@react-spring/web";
 
 interface BarItem {
   id: string;
@@ -30,30 +31,58 @@ export const Bar = ({ items }: { items: BarItem[] }) => {
     [maxValue, isInView]
   );
 
+  const [showMore, setShowMore] = useState<boolean>(false);
+
+  const tableStyles = useSpring({
+    height: showMore ? ref.current?.scrollHeight : 500,
+    config: {
+      duration: 1000,
+    },
+  });
+
+  const buttonStyles = useSpring({
+    opacity: showMore ? 0 : 1,
+    config: {
+      duration: 200,
+    },
+  });
+
   return (
-    <table ref={ref} className="w-full">
-      <tbody>
-        {items.map((item) => (
-          <tr
-            key={item.id}
-            className="flex flex-col justify-center gap-1 md:flex-row md:items-center"
-          >
-            <td className="pb-2 md:w-[40%] md:text-right">{item.label}</td>
-            <td className="flex w-full items-center gap-1 self-end pb-2">
-              <span
-                className="inline-block h-4 w-4 rounded bg-gradient-to-tr from-yellow-500 to-yellow-300 transition-[width] delay-300 duration-500"
-                style={style(item)}
-              />
-              <span
-                className="text-inherit opacity-0 delay-300 duration-200"
-                style={isInView ? { opacity: 1 } : undefined}
-              >
-                {item.value}
-              </span>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <animated.div style={tableStyles} className="relative overflow-hidden">
+      <table ref={ref} className="w-full">
+        <tbody>
+          {items.map((item) => (
+            <tr
+              key={item.id}
+              className="flex flex-col justify-center gap-1 md:flex-row md:items-center"
+            >
+              <td className="pb-2 md:w-[40%] md:text-right">{item.label}</td>
+              <td className="flex w-full items-center gap-1 self-end pb-2">
+                <span
+                  className="inline-block h-4 w-4 rounded bg-gradient-to-tr from-yellow-500 to-yellow-300 transition-[width] delay-300 duration-500"
+                  style={style(item)}
+                />
+                <span
+                  className="text-inherit opacity-0 delay-300 duration-200"
+                  style={isInView ? { opacity: 1 } : undefined}
+                >
+                  {item.value}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <animated.div
+        className={`absolute bottom-0 flex h-16 w-full items-center justify-center bg-gradient-to-t from-slate-400 ${
+          showMore && "pointer-events-none"
+        }`}
+        style={buttonStyles}
+      >
+        <button className="h-full w-full" onClick={() => setShowMore(true)}>
+          show more
+        </button>
+      </animated.div>
+    </animated.div>
   );
 };
